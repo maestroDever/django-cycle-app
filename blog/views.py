01efd921196814bfb0784fea9a38e6ea579daedb
maestroDever <dream.big.dev.423@gmail.com>
2019-08-29 19:20:21 -0500

    like button

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Blog, Category
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
 
 def index(request):
   template_name = "index.html"
+
+  if request.is_ajax():
+    is_liked = request.POST.get('like')
+    id = request.POST.get('id')
+    X = Blog.objects.get(id=id)
+    if is_liked == 'true':
+      X.favorite += 1
+    else:
+      X.favorite -= 1
+    X.save()
+    return JsonResponse({'message' : 'success'})
   blogs = Blog.objects.all()
   context = {
       "blogs": blogs
